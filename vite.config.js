2b2a973134f68d95234774d6d21654590062247d
Rishank99 <ue204067.rishank.eee.com>
2025-07-30 18:42:43 +0530

    changes made

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/ptekaro/",
